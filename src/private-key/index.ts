c8daf5e57a480ed0193ab734e7e64842aef3cdc7
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:49:00 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@909bd84

--- src/private-key/index.ts
+++ src/private-key/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/tls/4.1.0/docs/resources/private_key
 // generated from terraform resource schema
 
